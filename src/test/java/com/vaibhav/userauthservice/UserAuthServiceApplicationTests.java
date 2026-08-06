a28cda50961004c7407ea8335967c952f2b5f405
Vaibhav Nigam <vaibnigam404@gmail.com>
2026-08-06 22:15:21 +0530

    Initial project setup for user-auth-service

---
+++ src/test/java/com/vaibhav/userauthservice/UserAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaibhav.userauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
